6359dfab7de99333af573781ac7a00aeb8bc10e2
jeonhyeji <6887>
2021-05-03 12:47:15 +0900

    Initial commit

---
+++ src/main/java/com/dhk22/batchtutorials/BatchTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.dhk22.batchtutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchTutorialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhk22/batchtutorials/BatchTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhk22.batchtutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
